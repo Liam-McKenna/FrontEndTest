867dc1dbcbe779c6ff3c8a4a5978a05316b110ce
Liam-McKenna <liam.zendesign@gmail.com>
2021-08-23 17:41:08 +0100

    mobile v6

--- components/infoCard.php
+++ components/infoCard.php
@@ -1,70 +1,70 @@
 <html>
 <div class="cards-container">
-    <div class="card-line">
-        <div class="infocard-container">
-            <img src="./img/inforcardIcon1.svg" alt="">
-            <div class="infocard-text">
-                <h3>Adresse professionnelle</h3>
-                <div class="divider"></div>
-                <span>
-                    Vous rêvez d’avoir votre adresse sur les Champs- Élysées ? Profitez de notre sélection d’adresse pour choisir celle qui vous plaît !
-                </span>
-            </div>
+
+    <div class="infocard-container">
+        <img src="./img/inforcardIcon1.svg" alt="">
+        <div class="infocard-text">
+            <h3>Adresse professionnelle</h3>
+            <div class="divider"></div>
+            <span>
+                Vous rêvez d’avoir votre adresse sur les Champs- Élysées ? Profitez de notre sélection d’adresse pour choisir celle qui vous plaît !
+            </span>
         </div>
-        <div class="infocard-container">
-            <img src="./img/inforcardIcon1.svg" alt="">
-            <div class="infocard-text">
-                <h3>Toutes les formes juridiques</h3>
-                <div class="divider"></div>
-                <span>
-                    Peu importe votre forme juridique vous avez accès à nos service, SAS, SASU, Auto-entrepreneurs, SCI, professions libérales et même les associations.
-                </span>
-            </div>
+    </div>
+    <div class="infocard-container">
+        <img src="./img/inforcardIcon1.svg" alt="">
+        <div class="infocard-text">
+            <h3>Toutes les formes juridiques</h3>
+            <div class="divider"></div>
+            <span>
+                Peu importe votre forme juridique vous avez accès à nos service, SAS, SASU, Auto-entrepreneurs, SCI, professions libérales et même les associations.
+            </span>
         </div>
-        <div class="infocard-container">
-            <img src="./img/inforcardIcon1.svg" alt="">
-            <div class="infocard-text">
-                <h3>Plus de 7 500 domiciliations</h3>
-                <div class="divider"></div>
-                <span>
-                    Aujourd’hui nous faisons profiter plus de 7 500 professionnels de notre service de domiciliation et de nos nombreuses adresses.
-                </span>
-            </div>
+    </div>
+    <div class="infocard-container">
+        <img src="./img/inforcardIcon1.svg" alt="">
+        <div class="infocard-text">
+            <h3>Plus de 7 500 domiciliations</h3>
+            <div class="divider"></div>
+            <span>
+                Aujourd’hui nous faisons profiter plus de 7 500 professionnels de notre service de domiciliation et de nos nombreuses adresses.
+            </span>
         </div>
     </div>
 
-    <div class="card-line">
-        <div class="infocard-container">
-            <img src="./img/inforcardIcon1.svg" alt="">
-            <div class="infocard-text">
-                <h3>Adresse professionnelle</h3>
-                <div class="divider"></div>
-                <span>
-                    Vous rêvez d’avoir votre adresse sur les Champs- Élysées ? Profitez de notre sélection d’adresse pour choisir celle qui vous plaît !
-                </span>
-            </div>
-        </div>
-        <div class="infocard-container">
-            <img src="./img/inforcardIcon1.svg" alt="">
-            <div class="infocard-text">
-                <h3>Toutes les formes juridiques</h3>
-                <div class="divider"></div>
-                <span>
-                    Peu importe votre forme juridique vous avez accès à nos service, SAS, SASU, Auto-entrepreneurs, SCI, professions libérales et même les associations.
-                </span>
-            </div>
-        </div>
-        <div class="infocard-container">
-            <img src="./img/inforcardIcon1.svg" alt="">
-            <div class="infocard-text">
-                <h3>Plus de 7 500 domiciliations</h3>
-                <div class="divider"></div>
-                <span>
-                    Aujourd’hui nous faisons profiter plus de 7 500 professionnels de notre service de domiciliation et de nos nombreuses adresses.
-                </span>
-            </div>
+
+
+    <div class="infocard-container">
+        <img src="./img/inforcardIcon1.svg" alt="">
+        <div class="infocard-text">
+            <h3>Sans engagement</h3>
+            <div class="divider"></div>
+            <span>
+                Profitez de tous les services que peut vous offrir Domicilier-Entreprise sans aucun engagement ni aucune contrainte !
+            </span>
         </div>
     </div>
+    <div class="infocard-container">
+        <img src="./img/inforcardIcon1.svg" alt="">
+        <div class="infocard-text">
+            <h3>Gestion de votre courrier</h3>
+            <div class="divider"></div>
+            <span>
+                Nous mettons à disposition des professionnels qui nous font confiance un service de gestion de courrier sur mesure.
+            </span>
+        </div>
+    </div>
+    <div class="infocard-container">
+        <img src="./img/inforcardIcon1.svg" alt="">
+        <div class="infocard-text">
+            <h3>96% de satisfaction</h3>
+            <div class="divider"></div>
+            <span>
+                Notre service de domiciliation nous permet de satisfaire plus de 96% de nos clients qui nous font confiance pour leur avenir professionnel.
+            </span>
+        </div>
+    </div>
+
 </div>
 
 </html>
